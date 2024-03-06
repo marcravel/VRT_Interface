017ffe80ef8c8a46dda2964661a8c7624295b23c
Marc Ravelontsalama <marc.ravelontsalama@gmail.com>
2024-03-06 18:16:02 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace VRT_Interface
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShowProd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnShowProd
+            // 
+            this.btnShowProd.Location = new System.Drawing.Point(58, 173);
+            this.btnShowProd.Name = "btnShowProd";
+            this.btnShowProd.Size = new System.Drawing.Size(141, 44);
+            this.btnShowProd.TabIndex = 0;
+            this.btnShowProd.Text = "Show Prod";
+            this.btnShowProd.UseVisualStyleBackColor = true;
+            this.btnShowProd.Click += new System.EventHandler(this.btnShowProd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnShowProd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShowProd;
+    }
+}
+
---
+++ Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VRT_Interface
+{
+    internal class Product
+    {
+        public int Price { get; set; }
+        public string Name { get; set; }
+
+        public double sell(double qty)
+        {
+            return this.Price * qty;
+        }
+    }
+}
